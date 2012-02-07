3a1185b4e15f75c6040c4347d27b5dddbfdb9e3d
Wes Bailey <baywes@gmail.com>
2012-02-06 21:07:39 -0800

    wish there was a better way to test the code that relies on ARGV

--- lib/claws/options.rb
+++ lib/claws/options.rb
@@ -14,10 +14,22 @@ module Claws
       OptionParser.new do |opts|
         opts.banner = "Usage: script/aws [options] [environment] [role]"
 
-        opts.on('-s', '--status', 'display host status only and exit') do
+        opts.on('-d', '--display-only', 'display host information only and exit') do
+          options.connect = false
+        end
+
+        opts.on('-c', '--choice N', Float, 'enter the identity number of the host to automatically connect to') do |n|
+          options.choice = n.to_i
+        end
+
+        opts.on('-s', '--source', 'define the AWS source - default is ec2') do
+          options.source = 'elb'
           options.connect = false
         end
       end.parse!
+
+      options.environment = ARGV.shift
+      options.role = ARGV.shift
 
       options
     end
--- spec/claws_options_spec.rb
+++ spec/claws_options_spec.rb
@@ -1,27 +1,73 @@
 require 'spec_helper'
 require 'claws/options'
 
+ARGV.clear
+
+def cli(args)
+  ARGV.push(*args)
+  yield
+  ARGV.clear
+end
+
 describe Claws::Options do
   it 'defines default options' do
-    options = OpenStruct.new(
-      {
-        :connect => true,
-        :source => 'ec2',
-        :choice => nil,
-      }
-    )
-
-    Claws::Options.parse.should == options
+    cli %w{production app} do
+      options = Claws::Options.parse
+      options.connect.should be_true
+      options.source.should == 'ec2'
+      options.choice.should be_nil
+    end
   end
 
-  it 'accepts status only flag' do
+  it 'accepts display only flag' do
     # By default we want to connect to the instance
     Claws::Options.parse.connect.should be_true
 
     # Allow the user to override and just display information
-    ARGV = %w{script/claws -s}
-    Claws::Options.parse.connect.should be_false
-    ARGV = %w{script/claws --status}
-    Claws::Options.parse.connect.should be_false
+    cli %w{-d} do
+      Claws::Options.parse.connect.should be_false
+    end
+
+    cli %w{--display-only} do
+      Claws::Options.parse.connect.should be_false
+    end
+  end
+
+  it 'accepts a choice flag' do
+    cli %w{-c 10} do
+      Claws::Options.parse.choice.should == 10
+    end
+
+    cli %w{--choice 10} do
+      Claws::Options.parse.choice.should == 10
+    end
+  end
+
+  it 'accepts a source flag' do
+    cli %w{-s elb} do
+      options = Claws::Options.parse
+      options.source.should == 'elb'
+      options.connect.should be_false
+    end
+
+    cli %w{--source elb} do
+      options = Claws::Options.parse
+      options.source.should == 'elb'
+      options.connect.should be_false
+    end
+  end
+
+  context 'capistrano' do
+    it 'defines a environment' do
+      cli %w{-s production app} do
+        Claws::Options.parse.environment.should == 'production'
+      end
+    end
+
+    it 'defines the role' do
+      cli %w{-s production app} do
+        Claws::Options.parse.role.should == 'app'
+      end
+    end
   end
 end
