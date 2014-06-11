8e56e6487a89cab703c7b2d85b29ccf3293e4bbe
Wes Bailey <baywes@gmail.com>
2014-06-10 23:14:59 -0700

    working to update test to rspec 3.0

--- spec/options_spec.rb
+++ spec/options_spec.rb
@@ -11,58 +11,58 @@ describe Claws::Options do
   it 'defines default options' do
     cli %w{production app} do
       options = Claws::Options.parse
-      options.connect.should be_true
-      options.source.should == 'ec2'
-      options.selection.should be_nil
+      expect(options.connect).to be(true)
+      expect(options.source).to eq('ec2')
+      expect(options.selection).to be(nil)
     end
   end
 
   it 'accepts display only flag' do
     # By default we want to connect to the instance
-    Claws::Options.parse.connect.should be_true
+    expect(Claws::Options.parse.connect).to be(true)
 
     # Allow the user to override and just display information
     cli %w{-d} do
-      Claws::Options.parse.connect.should be_false
+      expect(Claws::Options.parse.connect).to be(false)
     end
 
     cli %w{--display-only} do
-      Claws::Options.parse.connect.should be_false
+      expect(Claws::Options.parse.connect).to be(false)
     end
   end
 
   it 'accepts a choice flag' do
     cli %w{-c 10} do
-      Claws::Options.parse.selection.should == 10
+      expect(Claws::Options.parse.selection).to eq(10)
     end
 
     cli %w{--choice 10} do
-      Claws::Options.parse.selection.should == 10
+      expect(Claws::Options.parse.selection).to eq(10)
     end
   end
 
   it 'accepts a source flag' do
     cli %w{-s elb} do
       options = Claws::Options.parse
-      options.source.should == 'elb'
+      expect(options.source).to eq('elb')
     end
 
     cli %w{--source elb} do
       options = Claws::Options.parse
-      options.source.should == 'elb'
+      expect(options.source).to eq('elb')
     end
   end
 
   context 'capistrano' do
     it 'defines the environment' do
       cli %w{production app} do
-        Claws::Options.parse.environment.should == 'production'
+        expect(Claws::Options.parse.environment).to eq('production')
       end
     end
 
     it 'defines the role' do
       cli %w{production app} do
-        Claws::Options.parse.role.should == 'app'
+        expect(Claws::Options.parse.role).to eq('app')
       end
     end
   end
